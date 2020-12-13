fec2a6b6085a54b8881be034366bff27e89c3b9b
wolwire <agrawal.mayank1996@gmail.com>
2020-12-14 00:30:39 +0530

    Fix the directory show method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from flask import Flask, request, render_template, flash, get_flashed_messages
 
 app = Flask(__name__)
-basepath = "/Users/mayank/"
+basepath = "/Users/mayank"
 
 def check_directory(path):
     if not(os.path.exists(path)):
@@ -17,34 +17,39 @@ def get_path(basepath, page_location):
         path = basepath + page_location
     else:
         path = basepath
-
-    if not(check_directory(path)):
-        raise Exception("not a correct path")
     return path
 
 @app.route('/')
 def list_directories():
     dir_names = []
     file_name = []
-    try:
-        page_location = request.args.get("page-location")
-        current_path = get_path(basepath, page_location)
-        for file in os.listdir(current_path):
-            if os.path.isfile(os.path.join(basepath, file)):
-                file_name.append(file)
-            else:
-                dir_names.append(file)
+    page_location = request.args.get("page-location")
+    page_location = page_location if page_location else ""
+    current_path = get_path(basepath, page_location)
 
-        header = page_location if page_location else 'Home'
-
-        details = {'file_name': file_name,
-                   'dir_names': dir_names,
-                   'header': header}
-
-        return render_template("files.html", details=details)
-    except Exception:
-        flash("Something went wrong")
+    if not(check_directory(current_path)):
+        flash(f"Wrong Directory {current_path}")
         return render_template("files.html")
+
+    if os.path.isfile(current_path):
+        flash(f"This is a file {current_path}")
+        return render_template("files.html")
+
+    for file in os.listdir(current_path):
+        if os.path.isfile(os.path.join(basepath, file)):
+            file_name.append(file)
+        else:
+            dir_names.append(file)
+
+    header = page_location if page_location else 'Home'
+
+    details = {'prev_path': page_location,
+               'file_name': file_name,
+               'dir_names': dir_names,
+               'header': header}
+
+    return render_template("files.html", details=details)
+
 
 
 if __name__ == '__main__':
