ac2487b6d850c7945e732ffbb10185a0f31bbee0
wolwire <agrawal.mayank1996@gmail.com>
2020-12-14 11:55:14 +0530

    Add feature to play video

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import re
-from flask import Flask, request, render_template, flash, get_flashed_messages
+from flask import Flask, request, render_template, flash, get_flashed_messages, url_for
 
 app = Flask(__name__)
 basepath = "/Users/mayank"
 
 def check_directory(path):
-    if not(os.path.exists(path)):
+    if not(os.path.exists(path)) and re.match(f"^{basepath}", path):
         return False
     return True
 
@@ -19,40 +19,54 @@ def get_path(basepath, page_location):
         path = basepath
     return path
 
+
 @app.route('/')
 def list_directories():
-    dir_names = []
-    file_name = []
+    file_details = []
     page_location = request.args.get("page-location")
     page_location = page_location if page_location else ""
     current_path = get_path(basepath, page_location)
 
     if not(check_directory(current_path)):
-        flash(f"Wrong Directory {current_path}")
+        flash(f"Wrong Directory")
         return render_template("files.html")
+
 
     if os.path.isfile(current_path):
         flash(f"This is a file {current_path}")
         return render_template("files.html")
 
     for file in os.listdir(current_path):
-        if os.path.isfile(os.path.join(basepath, file)):
-            file_name.append(file)
+        if file.lower().endswith(('.mkv', '.mp4', '.mpeg', '.mov')):
+            details = {"name": file, "path": f"/play?page-location={page_location}/{file}"}
         else:
-            dir_names.append(file)
+            details = {"name": file, "path": f"/?page-location={page_location}/{file}"}
+        file_details.append(details)
 
     header = page_location if page_location else 'Home'
 
-    details = {'prev_path': page_location,
-               'file_name': file_name,
-               'dir_names': dir_names,
+    details = {'file_details': file_details,
                'header': header}
 
     return render_template("files.html", details=details)
 
 
+@app.route('/play')
+def play_videos():
+    page_location = request.args.get("page-location")
+    page_location = page_location if page_location else ""
+    current_path = get_path(basepath, page_location)
+
+    if os.path.isfile(current_path) and current_path.lower().endswith(('.mkv', '.mp4', '.mpeg', '.mov')):
+        details = {"page_location": page_location, "base": basepath}
+        return render_template("video.html", details=details)
+    else:
+        flash(f"This is a file {current_path}")
+        return render_template("video.html")
+
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
+    app.add_url_rule(f"/{basepath}/<path:filename>", endpoint='css', view_func=app.send_static_file)
     app.run(debug=True, host='0.0.0.0', port=4000)
